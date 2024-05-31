c1cdf0823062e149c8c4458ad2e3ddca35d1fee4
Pratham Gupta <pratham36gupta@gmail.com>
2024-05-31 14:25:45 -0700

    Added broken-repl challenge

---
+++ hsctf2019/broken-repl/repl.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+with open("flag.txt") as flag: # open flag file
+    flag = flag.read() # read contents of flag file
+try: # make sure we don't run out of memory
+    while 1: # do this forever
+        try: # try to read a line of input
+            line = input(">>> ") # prompt is python's standard prompt
+        except EOFError: # user is done typing input
+            print() # ensure there is a line-break
+            break # exit from the loop
+        else: # successfully read input
+            try: # try to compile the input
+                code = compile(line, "<input>", "exec") # compile the line of input
+            except (OverflowError, SyntaxError, ValueError, TypeError, RecursionError) as e: # user input was bad
+                print("there was an error in your code:", e) # notify the user of the error
+            if False: exec(code) # run the code
+            # TODO: find replacement for exec
+            # TODO: exec is unsafe
+except MemoryError: # we ran out of memory
+    # uh oh
+    # lets remove the flag to clear up some memory
+    print(flag) # log the flag so it is not lost
+    del flag # delete the flag
+    # hopefully we have enough memory now
