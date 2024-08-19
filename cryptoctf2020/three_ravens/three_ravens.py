ace8177dd86d2924addbec8ba8efa62253b7f33a
Pratham Gupta <pratham36gupta@gmail.com>
2024-08-19 12:07:28 -0700

    Added three_ravens

---
+++ cryptoctf2020/three_ravens/three_ravens.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+from Crypto.Util.number import *
+# from flag import flag
+with open('/flag', 'r') as file:
+    flag = file.read().strip()
+
+def keygen(nbit):
+	while True:
+		p, q, r = [getPrime(nbit) for _ in range(3)]
+		if isPrime(p + q + r):
+			pubkey = (p * q * r, p + q + r)
+			privkey = (p, q, r)
+			return pubkey, privkey
+
+def encrypt(msg, pubkey):
+	enc = pow(bytes_to_long(msg.encode('utf-8')), 0x10001, pubkey[0] * pubkey[1])
+	return enc
+
+nbit = 512
+pubkey, _ = keygen(nbit)
+print('pubkey =', pubkey)
+
+enc = encrypt(flag, pubkey)
+print('enc =', enc)
