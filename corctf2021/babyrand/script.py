69fe0a282b9e12d7ad79a137b838ab205700143f
Pratham Gupta <pratham36gupta@gmail.com>
2024-08-27 18:43:37 -0700

    Added babyrand

---
+++ corctf2021/babyrand/script.py
@@ -0,0 +1,28 @@
+from random import randrange
+from Crypto.Util.number import getPrime, long_to_bytes
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from hashlib import sha256
+from os import urandom
+
+with open('/flag', 'r') as file:
+    flag = file.read().strip()
+
+def und():
+  p = getPrime(512)
+  x = randrange(p)
+  a = p ^ x ^ randrange(2**200)
+  b = p ^ x ^ randrange(2**200)
+  return p, a, b, x
+
+p,a,b,x = und()
+
+iv = urandom(16)
+key = sha256(long_to_bytes(a) + long_to_bytes(b)).digest()[:16]
+cipher = AES.new(key, AES.MODE_CBC, iv)
+
+print(f"c1 = {x}")
+print(f"c2 = {(x*a + b) % p}")
+print(f"p = {p}")
+print(f"iv = '{iv.hex()}'")
+print(f"ct = '{cipher.encrypt(pad(flag.encode(), 16)).hex()}'")
