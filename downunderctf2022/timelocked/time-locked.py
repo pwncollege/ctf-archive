cba85bdd40d15ddc8b5a2fb5fb2c82f1d9686754
Adical <90729099+Aditya9113@users.noreply.github.com>
2024-09-16 15:17:28 -0700

    Update time-locked.py

--- downunderctf2022/timelocked/time-locked.py
+++ downunderctf2022/timelocked/time-locked.py
@@ -1,9 +1,10 @@
 #!/opt/pwn.college/python
+
 from hashlib import sha256
 from Crypto.Util.Padding import unpad
 from Crypto.Cipher import AES
 
-
+flag = open("/flag").read.().strip().tohex()
 ct = bytes.fromhex('85534f055c72f11369903af5a8ac64e2f4cbf27759803041083d0417b5f0aaeac0490f018b117dd4376edd6b1c15ba02')
 
 
