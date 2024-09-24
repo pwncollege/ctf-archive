f9bb5ad7b85188a207a687c2737894bc5d341b61
Pratham Gupta <pratham36gupta@gmail.com>
2024-09-24 00:52:56 -0700

    Added gen.py

---
+++ uiuctf2024/determined/gen.py
@@ -0,0 +1,12 @@
+#!/opt/pwn.college/python
+
+from SECRET import FLAG, p, q, r
+from Crypto.Util.number import bytes_to_long
+n = p * q
+e = 65535
+m = bytes_to_long(FLAG)
+c = pow(m, e, n)
+# printed to gen.txt
+print(f"{n = }")
+print(f"{e = }")
+print(f"{c = }")
