a553fcf37cc7faf4a3f81e9811b0f12ac916fa49
Pratham Gupta <pratham36gupta@gmail.com>
2024-09-30 14:11:38 -0700

    Added quadratic leak challenge

---
+++ m0leconteaserctf2025/quadratic/QuadraticLeak.py
@@ -0,0 +1,22 @@
+#!/opt/pwn.college/python
+
+from Crypto.PublicKey import RSA
+from Crypto.Util.number import long_to_bytes, bytes_to_long
+
+flag = open("/flag","r").read().strip().encode()
+flag = bytes_to_long(flag)
+
+key = RSA.generate(2048)
+n = key.n
+e = key.e
+p, q = key.p, key.q
+
+leak = (p**2 + q**2 - p - q)%key.n
+
+ciph = pow(flag, key.e, key.n)
+ciph = long_to_bytes(ciph)
+
+print(f'{n = }')
+print(f'{e = }')
+print(f'{ciph = }')
+print(f'{leak = }')
