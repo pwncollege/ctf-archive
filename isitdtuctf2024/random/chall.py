bb3204817b8337d2e0b8356a27dbd50ae9ebc638
Pratham Gupta <pratham36gupta@gmail.com>
2024-11-04 17:28:47 -0700

    changes chall.py to write

--- isitdtuctf2024/random/chall.py
+++ isitdtuctf2024/random/chall.py
@@ -6,4 +6,5 @@ flag  = random.randbytes(random.randint(13, 1337))
 flag += FLAG
 flag += random.randbytes(random.randint(13, 1337))
 random.seed(flag)
-print(len(flag) < 1337*1.733 and [random.randrange(0, int(0x13371337*1.337)) for _ in range(0x13337)])
+f = open("/challenge/output.txt", "w")
+f.write(print(len(flag) < 1337*1.733 and [random.randrange(0, int(0x13371337*1.337)) for _ in range(0x13337)]))
