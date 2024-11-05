17b6488687690ac900894458988c96db191d94e0
Pratham Gupta <pratham36gupta@gmail.com>
2024-11-04 17:25:54 -0700

    Updated chall.py

--- isitdtuctf2024/random/chall.py
+++ isitdtuctf2024/random/chall.py
@@ -1,7 +1,9 @@
-#!/opt/pwn.college/python
+#!/run/workspace/bin/python
 import random
+from secret import FLAG
 flag  = random.randbytes(random.randint(13, 1337))
-flag += open("/flag", "rb").read()
+#flag += open("/flag", "rb").read()
+flag += FLAG
 flag += random.randbytes(random.randint(13, 1337))
 random.seed(flag)
 print(len(flag) < 1337*1.733 and [random.randrange(0, int(0x13371337*1.337)) for _ in range(0x13337)])
