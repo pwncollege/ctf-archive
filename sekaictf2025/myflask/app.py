a49021f7aafec7ec147ca0a1384b47fd54d0c3f2
Pratham Gupta <pratham36gupta@gmail.com>
2025-08-19 11:49:18 -0700

    Added sekaictf2025 flask challenge

---
+++ sekaictf2025/myflask/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/exec-suid -- /usr/bin/python3
+
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/view')
+def view():
+    filename = request.args.get('filename')
+    if not filename:
+        return "Filename is required", 400
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+        return content, 200
+    except FileNotFoundError:
+        return "File not found", 404
+    except Exception as e:
+        return f"Error: {str(e)}", 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
